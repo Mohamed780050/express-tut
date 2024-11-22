5765992bc4e3580833a2fea9511783c6fa1d33a9
Mohamed780050 <mohamed39omer@gmail.com>
2024-11-22 19:39:32 +0200

    added get response to the app file

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-import express from "express"
+import express from "express";
+import dotenv from "dotenv";
+import path from "path";
+
+dotenv.config();
+const app = express();
+const port = process.env.port;
+
+app.get("^/$|home(.html)?", (req, res) => {
+  res.sendFile(path.join(process.cwd(), "view/home.html"));
+});
+
+app.listen(port, () => console.log(`working on http://localhost:${port}`));
